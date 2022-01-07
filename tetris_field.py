2c7f514bebb2ae156abeadffb6ad43680e0c97d2
Snpyyy <snpyyy.a@gmail.com>
2022-01-07 09:35:58 +0900

    create field

---
+++ tetris_field.py
@@ -0,0 +1,7 @@
+class field:
+    def __init__(self, size):
+        self.window_width = size * 14
+        self.window_height = size * 20
+        self.canvas_width = size * 10
+        self.canvas_height = size * 20
+        self.one_block = size
