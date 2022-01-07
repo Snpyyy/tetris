59126ec5de849012e2ebc51b34d700ca12a671e1
Snpyyy <snpyyy.a@gmail.com>
2022-01-07 11:34:55 +0900

    new

--- tetris.py
+++ tetris.py
@@ -1,54 +1,11 @@
 # -*- coding:utf-8 -*-
 
-import tkinter
-from tetris_field import field
+import tkinter as tk
 
-root = tkinter.Tk()
-#---
+Size = 30
 
-# 1マスのサイズ（正方形）
-size = field(50)
+win = tk.Tk()
+can = tk.Canvas(win, width= 10*Size, height=20*Size )
+can.pack()
 
-# 画面作成
-root.title("テトリス！")
-root.geometry( str(size.window_width) + "x" + str(size.window_height) ) 
-root.config(bg="#ffffff")
-
-# キャンバス作成
-canvas = tkinter.Canvas(
-    root,
-    width= size.canvas_width,
-    height= size.canvas_height,
-    bg= "gray"
-)
-canvas.place(
-    x = size.one_block * 2 ,
-    y = 0,
-)
-
-# キャンバスのマス目作成 (後で変更=block_size)
-i = 1
-j = 1
-while i < 10:
-    canvas.create_rectangle(
-        size.one_block * i , 0, # 長方形のキャンバス上の始点座標
-        size.one_block * i, size.canvas_height, # 長方形のキャンバス上の終点座標
-        width = 0.1, # 枠線の太さ
-        outline = "white" # 塗り潰しの色
-    )
-    i += 1
-    while j < 20:
-        canvas.create_rectangle(
-            0, size.one_block * j, # 長方形のキャンバス上の始点座標
-            size.canvas_width, size.one_block * j, # 長方形のキャンバス上の終点座標
-            width = 0.1, # 枠線の太さ
-            outline = "white" # 塗り潰しの色
-        )
-        j += 1
-
-
-
-
-
-#---
-root.mainloop()
+win.mainloop()
--- tetris_field.py
+++
@@ -1,7 +0,0 @@
-class field:
-    def __init__(self, size):
-        self.window_width = size * 14
-        self.window_height = size * 20
-        self.canvas_width = size * 10
-        self.canvas_height = size * 20
-        self.one_block = size
